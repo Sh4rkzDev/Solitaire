af266fd5907319afc707e791635ad92d99c3f94a
sh4rk <lucasgarciacampos@gmail.com>
2023-10-20 17:12:15 -0300

    Adding tests for the Klondike Solitaire

---
+++ src/test/java/KFoundationTest.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class KFoundationTest {
+    @Test
+    public void testAddCard() {
+        KFoundation foundation = new KFoundation();
+        Card card = new Card(Suit.DIAMONDS, "A");
+        foundation.addCard(card, 0);
+        assertEquals(1, foundation.colSize(0));
+        assertEquals("A", foundation.getCard(0).getNum());
+        assertEquals(Suit.DIAMONDS, foundation.getCard(0).getSuit());
+    }
+
+    @Test
+    public void testSomeCards() {
+        KFoundation foundation = new KFoundation();
+        Card card = new Card(Suit.CLUBS, "A");
+        Card card1 = new Card(Suit.CLUBS, "2");
+        Card card2 = new Card(Suit.CLUBS, "3");
+        var aux = new ArrayList<Card>(Arrays.asList(card, card1, card2));
+        for (int i = 0; i < aux.size(); i++) {
+            foundation.addCard(aux.get(i), 2);
+            assertEquals(i + 1, foundation.colSize(2));
+            assertEquals(aux.get(i).getSuit(), foundation.getCard(2).getSuit());
+            assertEquals(aux.get(i).getNum(), foundation.getCard(2).getNum());
+        }
+    }
+}
---
+++ src/test/java/WasteTest.java
@@ -0,0 +1,63 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class WasteTest {
+
+    @Test
+    public void testOneCard() {
+        Waste waste = new Waste();
+        Card card = new Card(Suit.DIAMONDS, "K");
+        waste.addCard(card);
+        assertEquals(card.getNum(), waste.getCard().getNum());
+        assertEquals(card.getSuit(), waste.getCard().getSuit());
+        Card card1 = waste.removeCard();
+        assertEquals(card.getSuit(), card1.getSuit());
+        assertEquals(card.getNum(), card1.getNum());
+    }
+
+    @Test
+    public void testSomeCards() {
+        Waste waste = new Waste();
+        Card k = new Card(Suit.DIAMONDS, "K");
+        Card q = new Card(Suit.DIAMONDS, "Q");
+        Card j = new Card(Suit.DIAMONDS, "J");
+        Card ten = new Card(Suit.DIAMONDS, "10");
+        var aux = new ArrayList<Card>(Arrays.asList(k, q, j, ten));
+        for (Card card : aux) {
+            waste.addCard(card);
+            assertEquals(card.getNum(), waste.getCard().getNum());
+            assertEquals(card.getSuit(), waste.getCard().getSuit());
+        }
+        for (int i = aux.size() - 1; i > -1; i--) {
+            Card card = waste.removeCard();
+            assertEquals(aux.get(i).getSuit(), card.getSuit());
+            assertEquals(aux.get(i).getNum(), card.getNum());
+        }
+    }
+
+    @Test
+    public void testToDeck() {
+        Waste waste = new Waste();
+        Card k = new Card(Suit.DIAMONDS, "K");
+        Card q = new Card(Suit.DIAMONDS, "Q");
+        Card j = new Card(Suit.DIAMONDS, "J");
+        Card ten = new Card(Suit.DIAMONDS, "10");
+        var aux = new ArrayList<Card>(Arrays.asList(k, q, j, ten));
+        Deck deck1 = new Deck(aux);
+        for (int i = aux.size() - 1; i > -1; i--) {
+            waste.addCard(aux.get(i));
+        }
+        Deck deck2 = waste.toDeck();
+        while (deck1.isEmpty()) {
+            Card card1 = deck1.removeCard();
+            Card card2 = deck2.removeCard();
+            assertEquals(card1.getNum(), card2.getNum());
+            assertEquals(card1.getSuit(), card2.getSuit());
+        }
+        assertEquals(deck1.isEmpty(), deck2.isEmpty());
+    }
+}
