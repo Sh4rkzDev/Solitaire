cab6a5e1fde66c0e40036796ae6f05325ccc978d
sh4rk <lucasgarciacampos@gmail.com>
2023-11-19 17:14:01 -0300

    Adding images of cards

--- src/main/java/view/CardUI.java
+++ src/main/java/view/CardUI.java
@@ -1,4 +1,20 @@
 package view;
 
+import javafx.scene.canvas.Canvas;
+import model.Suit;
+
 public class CardUI {
+    private Canvas canvas;
+
+    public CardUI(String num, Suit suit) {
+        String strSuit = "";
+        switch (suit) {
+            case CLUBS -> strSuit = "C";
+            case DIAMONDS -> strSuit = "D";
+            case SPADES -> strSuit = "S";
+            case HEARTS -> strSuit = "H";
+        }
+        String path = (num.equals("10") ? "T" : num) + strSuit + ".svg";
+
+    }
 }
