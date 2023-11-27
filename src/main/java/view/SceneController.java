2e9f9572289da7b39277edbbf1e94a610b8d3f69
Tomas <caporalettitomas@gmail.com>
2023-11-27 20:25:14 -0300

    Primeros pasos menu, parte 2

--- src/main/java/view/SceneController.java
+++ src/main/java/view/SceneController.java
@@ -2,12 +2,16 @@ package view;
 
 import java.io.IOException;
 
+import controller.KlondikeController;
+import controller.SpiderController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.Klondike;
+import model.Spider;
 
 public class SceneController {
 
@@ -16,19 +20,15 @@ public class SceneController {
     private Parent root;
 
     public void switchToSpider(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/spider.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        Spider spd = new Spider((byte) 1);
+        SpiderUI view = new SpiderUI(stage, spd);
+        SpiderController controller = new SpiderController(view, spd);
     }
 
     public void switchToKlondike(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/klondike.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        Klondike kld = new Klondike((byte) 1);
+        KlondikeUI view = new KlondikeUI(stage, kld);
+        KlondikeController controller = new KlondikeController(view, kld);
     }
 
     public void switchToMenu(ActionEvent event) throws IOException {
