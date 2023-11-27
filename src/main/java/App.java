82f340644d177c5fd197d6a7a739db92f1a1c139
Tomas <caporalettitomas@gmail.com>
2023-11-27 20:16:15 -0300

    Primeros pasos menu

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,16 @@
 import controller.KlondikeController;
 import controller.SpiderController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.Klondike;
 import model.Spider;
 import view.KlondikeUI;
 import view.SpiderUI;
 
+/*
 public class App extends Application {
     @Override
     public void start(Stage stage) {
@@ -15,11 +19,32 @@ public class App extends Application {
         KlondikeController controller = new KlondikeController(view, kld);
     }
 
-    /*public void start(Stage stage) {
+    public void start(Stage stage) {
         Spider spd = new Spider((byte) 1);
         SpiderUI view = new SpiderUI(stage, spd);
         SpiderController controller = new SpiderController(view, spd);
     }
-     */
+
 
 }
+*/
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try {
+
+            Parent root = FXMLLoader.load(getClass().getResource("/menu.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
